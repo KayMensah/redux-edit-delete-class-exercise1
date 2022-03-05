00822b96446b9f2c4ec30b92e2f7a307c33a368a
Korkor Mensah <komensah2018@gmail.com>
2022-03-05 14:28:01 +0000

    redux edit delet class exercise

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from "./logo.svg";
+import React from "react";
+import UserForm from "./components/UserForm";
+import UserList from "./components/UserList";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <UserForm />
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
